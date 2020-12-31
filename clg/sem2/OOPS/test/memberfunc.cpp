eff671d56e0836f54cf26028b2ef070e19c5fede
m0hanram <55842167+m0hanram@users.noreply.github.com>
2020-12-31 11:49:25 +0530

    oops  ps

---
+++ clg/sem2/OOPS/test/memberfunc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"head.h"
+using namespace std;
+void event::input()
+{
+    p.input();
+    d.input();
+}
+void event::display()
+{
+    p.display();
+    d.display();
+}

fd4c48fcbbc5d351198f5511cd79efcc1e50010d
m0hanram <55842167+m0hanram@users.noreply.github.com>
2021-03-09 18:20:57 +0530

    commit profiles

--- clg/sem4/PYTHONLAB/package/socialnetwork/socialnetwork/settings.py
+++ clg/sem4/PYTHONLAB/package/socialnetwork/socialnetwork/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
